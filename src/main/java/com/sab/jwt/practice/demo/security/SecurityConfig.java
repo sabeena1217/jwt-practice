45e59d82c2adb844b579f702fcf15d9b999ee890
sabeena1217 <sabeena1217@gmail.com>
2021-08-01 10:59:53 +0530

    authorization done with access token

--- src/main/java/com/sab/jwt/practice/demo/security/SecurityConfig.java
+++ src/main/java/com/sab/jwt/practice/demo/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.sab.jwt.practice.demo.security;
 
 import com.sab.jwt.practice.demo.filter.CustomAuthenticationFilter;
+import com.sab.jwt.practice.demo.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
@@ -46,6 +48,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //        http.authorizeRequests().anyRequest().permitAll();
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
