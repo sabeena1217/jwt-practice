d641708aa4bb4ad7438dd6118cbf4dfdb13e5b01
sabeena1217 <sabeena1217@gmail.com>
2021-08-01 11:00:28 +0530

    authorization done with access token

---
+++ src/main/java/com/sab/jwt/practice/demo/filter/CustomAuthorizationFilter.java
@@ -0,0 +1,75 @@
+package com.sab.jwt.practice.demo.filter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Slf4j
+public class CustomAuthorizationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (request.getServletPath().equals("/api/v1/login")) {
+            // we are not gonna do anything if it is the login url
+            filterChain.doFilter(request, response);
+        } else {
+            // gonna authorize
+            String authorizationHeader = request.getHeader(AUTHORIZATION);
+            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+                try {
+                    String token = authorizationHeader.substring("Bearer ".length());
+
+                    // use the same algorithm that we used to encode the token
+                    // and pass it to the verifier
+                    Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+                    JWTVerifier verifier = JWT.require(algorithm).build();
+                    DecodedJWT decodedJWT = verifier.verify(token);
+
+                    String username = decodedJWT.getSubject();
+                    List<SimpleGrantedAuthority> roles = decodedJWT.getClaim("roles").asList(SimpleGrantedAuthority.class);
+
+                    // tells spring security here's the username, here are the roles
+                    // and here's what they can do with the application
+                    UsernamePasswordAuthenticationToken authenticationToken =
+                            new UsernamePasswordAuthenticationToken(username, null, roles);
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+                    filterChain.doFilter(request, response);
+
+                } catch (Exception e) {
+                    log.error("Error at authentication : {}", e.getMessage());
+
+                    response.setHeader("error", e.getMessage());
+                    response.setStatus(FORBIDDEN.value());
+
+                    Map<String, String> errors = new HashMap<>();
+                    errors.put("error_message",e.getMessage());
+                    response.setContentType(APPLICATION_JSON_VALUE);
+                    new ObjectMapper().writeValue(response.getOutputStream(),errors);
+                }
+            } else {
+                filterChain.doFilter(request, response);
+            }
+        }
+    }
+}
